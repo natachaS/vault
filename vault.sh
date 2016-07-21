d79ff091f3b7fc5e7d6df0d25d9b2e5a691e6524
Natacha <natacha@kickstarter.com>
2016-07-21 11:49:52 -0400

    added script

---
+++ vault.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+pkcs7=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/pkcs7 | paste -s -d '')
+result=$(curl -X POST "http://10.0.1.161:8200/v1/auth/aws-ec2/login" -d '{"role":"myapp","pkcs7":"'"$pkcs7"'","nonce":"vault-client-nonce"}')
+token=$( jq -r .auth.client_token <<< "$result" )
+policies=$( jq -r .auth.policies[] <<< "$result" )
+ami_id=$( jq -r .auth.metadata.ami_id <<< "$result" )
+[[ -n $token ]] || exit 1
+
+
+echo "Successfully authenticated:"
+echo "     Token: $token"
+echo "  Policies:" $policies
+echo "    AMI ID: $ami_id"
+
+echo "...testing out the auth....what is my favorite color ??? It's..."
+color=$(curl -s http://10.0.1.161:8200/v1/secret/example/favorites -H "X-Vault-Token:$token"| jq -r .data.color)
+echo $color
