9ed5c314ecd96c1e30870c1cd2ca594bbaa7f2af
Natacha <natachaS@users.noreply.github.com>
2016-09-07 16:04:12 -0400

    Updated script

--- vault.sh
+++ vault.sh
@@ -3,7 +3,7 @@
 set -e
 
 pkcs7=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/pkcs7 | paste -s -d '')
-result=$(curl -X POST "http://10.0.1.130:8200/v1/auth/aws-ec2/login" -d '{"role":"myapp","pkcs7":"'"$pkcs7"'","nonce":"vault-client-nonce"}')
+result=$(curl -X POST "http://172.31.138.175:8200/v1/auth/aws-ec2/login" -d '{"role":"app-env","pkcs7":"'"$pkcs7"'","nonce":"vault-client-nonce"}')
 token=$( jq -r .auth.client_token <<< "$result" )
 policies=$( jq -r .auth.policies[] <<< "$result" )
 ami_id=$( jq -r .auth.metadata.ami_id <<< "$result" )
@@ -13,8 +13,7 @@ ami_id=$( jq -r .auth.metadata.ami_id <<< "$result" )
 echo "Successfully authenticated:"
 echo "     Token: $token"
 echo "  Policies:" $policies
-echo "    AMI ID: $ami_id"
 
 echo "...testing out the auth....what is my favorite color ??? It's..."
-color=$(curl -s http://10.0.1.130:8200/v1/secret/example/favorites -H "X-Vault-Token:$token"| jq -r .data.color)
+color=$(curl -s http://172.31.138.175:8200/v1/secret/app/dev/favorites -H "X-Vault-Token:$token"| jq -r .data.color)
 echo $color
